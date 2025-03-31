877c87b6173b639f02f4abd99f2cb8dbe9a06b11
rraadd88 <rohanadandage@gmail.com>
2025-03-30 21:08:12 -0400

    Update test_examples.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,37 +7,37 @@ assert len(set(['modules','examples','tests']) - set(os.listdir('.')))==0
 assert exists('./modules/inputs')
 makedirs('./examples/outputs',exist_ok=True)
     
-@testbook('examples/chrov_viz_annot.ipynb', execute=True)
+@testbook('examples/viz_annot.ipynb', execute=True)
 def test_chrov_viz_annot(tb):
     pass # execute only because tests are present in the notebook itself
     return
 
-@testbook('examples/chrov_viz_chrom.ipynb', execute=True)
+@testbook('examples/viz_chrom.ipynb', execute=True)
 def test_chrov_viz_chrom(tb):
     pass # execute only because tests are present in the notebook itself
     return
 
-@testbook('examples/chrov_viz_chrom_custom.ipynb', execute=True)
+@testbook('examples/viz_chrom_custom.ipynb', execute=True)
 def test_chrov_viz_chrom_custom(tb):
     pass # execute only because tests are present in the notebook itself
     return
 
-@testbook('examples/chrov_viz_figure_custom.ipynb', execute=True)
+@testbook('examples/viz_figure_custom.ipynb', execute=True)
 def test_chrov_viz_figure_custom(tb):
     pass # execute only because tests are present in the notebook itself
     return
 
-@testbook('examples/chrov_viz_figure_stem.ipynb', execute=True)
+@testbook('examples/viz_figure_stem.ipynb', execute=True)
 def test_chrov_viz_figure_stem(tb):
     pass # execute only because tests are present in the notebook itself
     return
 
-@testbook('examples/chrov_viz_ranges.ipynb', execute=True)
+@testbook('examples/viz_ranges.ipynb', execute=True)
 def test_chrov_viz_ranges(tb):
     pass # execute only because tests are present in the notebook itself
     return
 
-@testbook('examples/chrov_viz_tests.ipynb', execute=True)
+@testbook('examples/viz_tests.ipynb', execute=True)
 def test_chrov_viz_tests(tb):
     pass # execute only because tests are present in the notebook itself
     return
